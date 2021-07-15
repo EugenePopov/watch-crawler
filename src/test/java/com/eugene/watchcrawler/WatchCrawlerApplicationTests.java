6ee13effc2a62f4c80fd0de63ed5a763ed422653
epopov <popov.eugene1994@gmail.com>
2021-07-15 11:45:05 +0300

    initial

---
+++ src/test/java/com/eugene/watchcrawler/WatchCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugene.watchcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

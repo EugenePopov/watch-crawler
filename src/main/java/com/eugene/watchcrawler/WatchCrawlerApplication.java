0d779125d4332c087ce71d0979dc234420ea37f0
epopov <popov.eugene1994@gmail.com>
2021-07-15 11:54:36 +0300

    utils

--- src/main/java/com/eugene/watchcrawler/WatchCrawlerApplication.java
+++ src/main/java/com/eugene/watchcrawler/WatchCrawlerApplication.java
@@ -1,13 +1,20 @@
 package com.eugene.watchcrawler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class WatchCrawlerApplication {
+public class WatchCrawlerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatchCrawlerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
